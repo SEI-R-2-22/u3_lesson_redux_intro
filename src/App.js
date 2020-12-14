2dc0ca2ff9dd1e17df42bd40bf218212b73e418c
Collin Didier <collin.didier80@gmail.com>
2020-12-14 17:00:08 -0600

    moved header to todolist

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './styles/App.css'
+import TodoList from "./components/TodoList"
 
 function App() {
   return (
     <div className="App">
-      <h3>Redux Intro</h3>
+      <TodoList />
     </div>
   )
 }
