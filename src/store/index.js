7385dbc9e12f8404ca963b6d99403665a97f18a5
Collin Didier <collin.didier80@gmail.com>
2020-12-14 16:59:28 -0600

    add indexjs

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,11 @@
-import { createStore } from 'redux'
+import { createStore, combineReducers } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import TodoReducer from './reducers/TodoReducer'
+import AppReducer from './reducers/AppReducer'
 
-const store = createStore(() => ({}))
+const store = createStore(
+    combineReducers({ todoState: TodoReducer, appState: AppReducer }),
+    composeWithDevTools()
+)
 
 export default store
