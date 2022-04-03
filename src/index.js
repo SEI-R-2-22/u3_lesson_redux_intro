a7235e52f25f262c6a007bf2dbf9afbbb2cc5227
John A. Jacobs <john.a.jacobs1973@gmail.com>
2022-04-03 17:07:10 -0400

    index.css import from styles dir

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {createRoot} from 'react-dom/client'
-import './index.css'
+import './styles/index.css'
 import App from './App'
 
 const root = createRoot(document.getElementById('root'))
